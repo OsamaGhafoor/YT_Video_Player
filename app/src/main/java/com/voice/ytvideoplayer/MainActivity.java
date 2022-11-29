1bd1cfbd646f211fed547c3e3a811ebbfc4c7d6d
OsamaGhafoor <osamaghafoor58@gmail.com>
2022-11-29 17:00:34 +0500

    Initial commit

---
+++ app/src/main/java/com/voice/ytvideoplayer/MainActivity.java
@@ -0,0 +1,36 @@
+package com.voice.ytvideoplayer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // https://github.com/PierfrancescoSoffritti/android-youtube-player
+
+        YouTubePlayerView youTubePlayerView = findViewById(R.id.youtube_player_view);
+        getLifecycle().addObserver(youTubePlayerView);
+
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                String videoId = "q_AOS8c1yx8";
+                youTubePlayer.loadVideo(videoId, 0);
+                youTubePlayerView.enterFullScreen();
+                youTubePlayerView.exitFullScreen();
+                youTubePlayerView.isFullScreen();
+                youTubePlayerView.toggleFullScreen();
+            }
+        });
+    }
+}
